29e196bb09d492f737da29920da2f88e75c938a3
fabriciodanioj <fabriciodanioj@gmail.com>
2020-04-17 04:10:20 -0300

    Reestructured structure based on SOLID

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,9 +1,38 @@
-import { Router } from 'express';
+import { Router, response } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentServices from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
+const appointmentsRepository = new AppointmentsRepository();
+
 appointmentsRouter.get('/', (req, res) => {
-  return res.send({ msg: 'Hello World' });
+  const appointments = appointmentsRepository.findAll();
+
+  return res.status(200).send(appointments);
+});
+
+appointmentsRouter.post('/', (req, res) => {
+  try {
+    const { provider, date } = req.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentServices(
+      appointmentsRepository,
+    );
+
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return res.send(appointment);
+  } catch (err) {
+    return res.status(400).send({ error: err.message });
+  }
 });
 
 export default appointmentsRouter;
